28f41eead616f55dd9483641e3fc1550bbf7b6cd
leomari <marihl@stud.ntnu.no>
2018-04-26 03:26:46 +0000

    settings

--- earthquake/settings.py
+++ earthquake/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 #DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost:8000','localhost','127.0.0.1','206.189.175.121']
 
 
 # Application definition
